6d016a221260cac518e9fa68f62142d9b8cb68ea
moaz <moazby@gmail.com>
2025-12-24 20:08:31 +0100

    Navbar Added for mobile screen

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/index";
-
+import Hero from "~/components/Hero";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -13,5 +13,7 @@ export default function Home() {
 
 
 
-  return <section>My App</section>;
+  return <section>
+    <Hero />
+  </section>;
 }
